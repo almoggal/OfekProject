15c6dddd4f6da378b50795a31160a3a2b6c6036e
Amit Cirt <siamitcs0@gmail.com>
2017-03-23 07:42:55 +0200

    Add bases with data functions

--- ofek-project/js/bases.js
+++ ofek-project/js/bases.js
@@ -1,32 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
     console.log("bases page ready!");
 
     //Populate list
     showSpinner(true);
     getBases($('#bases_list'));
-    
+
     //
 });
 
-var popluateList = function(list, bases){
-    for(var i = 0; i<bases.length; i++){
-        list[0].innerHTML+="<li><button class=\"w3-indigo w3-btn w3-round w3-xlarge\">"+bases[i]+"</button></li>";
+var popluateList = function (list, bases) {
+    for (var i = 0; i < bases.length; i++) {
+        list[0].innerHTML += "<li><button class=\"w3-indigo w3-btn w3-round w3-xlarge\">" + bases[i] + "</button></li>";
     }
 }
 
-var getBases = function(list){
-    readBaseNamesPromise().then(function(snapshot) {
-        var base_names = snapshot.val();
+var getBases = function (list) {
+    readBases().then(function (base_names) {
         console.log(JSON.stringify(base_names));
         popluateList(list, base_names);
         showSpinner(false);
-});
+    });
 }
 
-function showSpinner(f){
+function showSpinner(f) {
 
-    if(f)
-        $("#loading_spinner").attr("class","");
+    if (f)
+        $("#loading_spinner").attr("class", "");
     else
-        $("#loading_spinner").attr("class","w3-hide");
+        $("#loading_spinner").attr("class", "w3-hide");
 }
--- ofek-project/js/db_io.js
+++ ofek-project/js/db_io.js
@@ -1,12 +1,35 @@
 function writeBaseNames(base_names) {
     firebase.database().ref('base_names/').set({
-     base_name_list: base_names
+        base_name_list: base_names
     });
 }
 
 function writeBase(base_name, base_properties) {
     firebase.database().ref('bases/' + base_name).set({
         properties: base_properties
+    });
+    readBases().then(function (bases) {
+        var found = false;
+        var count = 0;
+        if (bases != null) {
+            if (Array.isArray(bases)) {
+                bases.forEach(function (base) {
+                    if (base_name == base) {
+                        found = true;
+                    }
+                });
+                count = bases.length;
+            }
+            else {
+                if (bases == base_name) {
+                    found == true;
+                }
+                count = 1;
+            }
+        }
+        if (!found) {
+            firebase.database().ref('bases_with_data/' + count + '/').set(base_name);
+        }
     });
 }
 
@@ -17,4 +40,11 @@ function readBaseNamesPromise() {
 
 function readBasePromise(base_name) {
     return firebase.database().ref('bases/' + base_name).once("value");
+}
+function readBases() {
+    return new Promise(function (resolve, reject) {
+        firebase.database().ref('bases_with_data/').once("value").then(function (bases) {
+            resolve(bases.val());
+        })
+    });
 }
